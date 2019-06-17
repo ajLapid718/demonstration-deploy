4935b1e12444fb493420845868be427930c63b0c
Allan James Lapid <ajLapid718@gmail.com>
2019-06-17 13:41:05 -0400

    Serve static

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.get("/students", (req, res, next) => {
   res.status(200).json([{id: 1, name: "Axel"}, {id:2, name: "Chris"}, {id: 3, name: "Michelle"}]);
 })
 
+let locationOfPublicFolder = path.join(__dirname, "client", "build")
+app.use(express.static(locationOfPublicFolder));
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!!!`);
 })
