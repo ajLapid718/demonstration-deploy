1c58713391de4967f1ecf92e75034c966130177c
Allan James Lapid <ajLapid718@gmail.com>
2019-06-17 12:12:32 -0400

    Handle production environment for port

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = 1234 || process.env.PORT;
 
 app.get("/students", (req, res, next) => {
   res.json([{id: 1, name: Abe}, {id: 2, name: Brian}, {id: 3, name: Aaron}]);
 })
 
-app.listen(1234, () => {
-  console.log("Listening on port 1234");
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
