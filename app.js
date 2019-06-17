12178d06b6fb53f0b2e7d35f0acaf29b37adb282
Allan James Lapid <ajLapid718@gmail.com>
2019-06-17 13:34:41 -0400

    Port dynamic

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const PORT = 1234;
+const PORT = process.env.PORT || 1234;
 
 app.get("/students", (req, res, next) => {
   res.status(200).json([{id: 1, name: "Axel"}, {id:2, name: "Chris"}, {id: 3, name: "Michelle"}]);
