5d3c3abcbf52c21fb7045a5a31d86744090b4a54
Allan James Lapid <ajLapid718@gmail.com>
2019-06-17 12:41:01 -0400

    Change dummy data

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 1234;
 
 app.get("/students", (req, res, next) => {
-  res.status(200).json("All of the students!!!");
+  res.status(200).json([{id: 1, name: Axel}, {id:2, name: Chris}, {id: 3, name: Michelle}]);
 })
 
 let locationOfPublicFolder = path.join(__dirname, "client", "build")
